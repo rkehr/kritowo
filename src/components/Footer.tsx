b7b1492ca754640d294e482d47aec302c6d4e045
s0556448 <s0556448@htw-berlin.de>
2025-10-13 11:02:37 +0200

    changed imprint link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className="px-8 py-2 flex items-center text-sm gap-2">
       <Copyleft className="inline opacity-20" />
       <span className="opacity-20">2025 | no cookies, no gods, no masters</span>
-      <Link href="/imprint"> Impressum </Link>
+      <Link href="/data-protection-agreement"> Impressum </Link>
       <Link href="/data-protection-agreement"> Datenschutzerklärung</Link>
     </div>
   );
